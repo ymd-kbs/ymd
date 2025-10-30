e322184fcce0b872aab71d83567cbed8fc0de97b
ymd <ymd@localhost>
2025-10-31 00:30:39 +0900

    Java 2.3

---
+++ HelloWorld_YamadaFumiya/src/sample/SelfIntroduction.java
@@ -0,0 +1,23 @@
+package sample;
+
+public class SelfIntroduction {
+
+	public static void main(String[] args) {
+	// 氏名
+	String name = "山田郁也";
+	// 年齢
+	int age =25;
+	// 趣味
+	String hobby = "猫を吸うこと";
+	
+	/*
+	 * 自己紹介文を出力する処理
+	 */
+	
+	System.out.println("私の名前は" + name + "です。");
+	System.out.println("年齢は" + age + "です。");
+	System.out.println("趣味は" + hobby + "です。");
+
+	}
+
+}
