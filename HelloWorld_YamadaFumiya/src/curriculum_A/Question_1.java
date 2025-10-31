c2411aed617e7aee399944d1c998d62af8700365
ymd-kbs <kabashiraezpz@gmail.com>
2025-11-01 04:42:08 +0900

    課題　Q1,Q2

---
+++ HelloWorld_YamadaFumiya/src/curriculum_A/Question_1.java
@@ -0,0 +1,47 @@
+package curriculum_A;
+
+public class Question_1 {
+
+	public static void main(String[] args) {
+		
+		// byte型の変数 byteNum を宣言
+		byte byteNum ;
+		// short型の変数 shortNum を宣言
+		short shortNum ;
+		// int型の変数 intNum を宣言
+		int intNum ;
+		// lonng型の変数 longNum を宣言
+		long longNum ;
+		// float型の変数 floatNum を宣言
+		float floatNum ;
+		// double型の変数 doubleNUm を宣言
+		double doubleNum ;
+		// char型の変数 letter を宣言
+		char letter ;
+		// String型の変数 letters を宣言
+		String letters ;
+		// boolean型の変数 Boolean を宣言
+		boolean isBoolean ;
+		
+		// 変数 byteNum に 10 を代入
+		byteNum = 10 ;
+		// 変数　shortNum に 100 を代入
+		shortNum = 100 ;
+		// 変数 intNum に 1000 を代入
+		intNum = 1000 ;
+		// 変数 longNum に 10000 を代入
+		longNum = 10000 ;
+		// 変数 floatNum に 9.5 を代入
+		floatNum = (float) 9.5 ;
+		// 変数 doubleNum に 10.5 を代入
+		doubleNum = 10.5 ;
+		// 変数 letter に 'a' を代入
+		letter = 'a' ;
+		// 変数 letters に "ハロー" を代入
+		letters = "ハロー" ;
+		// 変数 isBoolean に true を代入
+		isBoolean = true ;
+		
+	}
+
+}
--- HelloWorld_YamadaFumiya/src/sample/Sample.java
+++ HelloWorld_YamadaFumiya/src/sample/Sample.java
@@ -4,7 +4,6 @@ public class Sample {
 
 	public static void main(String[] args) {
 		System.out.println("Hello,World");
-		// TODO 自動生成されたメソッド・スタブ
 		}
 
 }
