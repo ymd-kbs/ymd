59222a17b8e22e80dad93453dd1762dcadcdf3a9
ymd-kbs <kabashiraezpz@gmail.com>
2025-11-18 23:32:39 +0900

    課題Q7〜Q10

--- HelloWorld_YamadaFumiya/src/curriculum_B/Question_4.java
+++ HelloWorld_YamadaFumiya/src/curriculum_B/Question_4.java
@@ -119,15 +119,103 @@ public class Question_4 {
 		// found が true の場合の出力
 		if (found) {
 			output = g + "は配列に含まれています";
-		// found が true では無い場合
+			// found が true では無い場合
 		} else {
 			output = g + "は配列に含まれていません";
 		}
 		// 結果を出力
 		System.out.println(output);
+		// scanner を閉じる
+		scanner.close();
 		// 改行
 		System.out.println();
-		
+
+		/*
+		 * [課題Q7] 2次元配列 int[][] array = {{1, 2}, {3, 4}, {5, 6}}; を用意する。配列の すべての要素を表示する。
+		 */
+		// int型の2次元配列を宣言し、初期化
+		int[][] array = { { 1, 2 }, { 3, 4 }, { 5, 6 } };
+		// 外側のループで 行 を処理( array.length の行数は 3 )
+		for (int i = 0; i < array.length; i++) {
+			// 内側のループで 列 を処理( array[i].length の要素数は 2 )
+			for (int j = 0; j < array[i].length; j++) {
+				// 全要素を順番に出力
+				System.out.print(array[i][j] + " ");
+			}
+			// 列の表示が終わったら改行
+			System.out.println();
+		}
+		// 改行
+		System.out.println();
+
+		/*
+		 * [課題Q8] 2次元配列 int[][] array = {{10, 20, 30}, {40, 50, 60}, {70, 80, 90}}; を用意する。配列の すべての要素の合計値を表示する。
+		 */
+		// int型の2次元配列を宣言し、初期化
+		int[][] array2 = { { 10, 20, 30 }, { 40, 50, 60 }, { 70, 80, 90 } };
+		// 合計値を格納する変数
+		int total = 0;
+		// 外側のループで 行 を処理( array2.length の行数は 3 )
+		for (int k = 0; k < array2.length; k++) {
+			// 内側のループで 列 を処理( array2[k].length の要素数は 3 )
+			for (int l = 0; l < array2[k].length; l++) {
+				// total に全要素を順番に加算していく
+				total += array2[k][l];
+			}
+		}
+		// total を出力
+		System.out.println(total);
+		// 改行
+		System.out.println();
+
+		/*
+		 * [課題Q9] 2次元配列 int[][] array = {{12, 15, 8}, {6, 19, 25}, {30, 2, 10}}; を用意する。配列内の 最大値と最小値 を求めて表示する。
+		 */
+		// int型の2次元配列を宣言し、初期化
+		int[][] array3 = { { 12, 15, 8 }, { 6, 19, 25 }, { 30, 2, 10 } };
+		// 最大値と最小値を配列の最初の要素で初期化（ 12 ）
+		int maximum = array3[0][0];
+		int minimum = array3[0][0];
+		// 外側のループで 行 を処理( array3.length の行数は 3 )
+		for (int m = 0; m < array3.length; m++) {
+			// 内側のループで 列 を処理( array3[m].length の要素数は 3 )
+			for (int n = 0; n < array3[m].length; n++) {
+				int o = array3[m][n];
+				// 最大値を更新
+				if (o > maximum) {
+					maximum = o;
+				}
+				// 最小値を更新
+				if (o < minimum) {
+					minimum = o;
+				}
+			}
+		}
+		// 最大値、最大値をそれぞれ出力
+		System.out.println("最大値:" + maximum);
+		System.out.println("最小値:" + minimum);
+		// 改行
+		System.out.println();
+
+		/*
+		 * [課題Q10] 3次元配列 int[][][] array = {{{1, 2}, {3, 4}}, {{5, 6}, {7, 8}}}; を用意する。配列内のすべての要素を表示する。
+		 */
+		// int型の3次元配列を宣言し、初期化
+		int[][][] array4 = { { { 1, 2 }, { 3, 4 } }, { { 5, 6 }, { 7, 8 } } };
+		// 外側のループで ブロック を処理( array4.length のブロック数は 2 )
+		for (int p = 0; p < array4.length; p++) {
+			System.out.println("ブロック[" + p + "]");
+			// 中央のループで 行 を処理( array4.length の行数は 2 )
+			for (int q = 0; q < array4[p].length; q++) {
+				System.out.print("行[" + q + "]");
+				// 内側のループで 列 を処理( array4.length の要素数は 2 )
+				for (int r = 0; r < array4[p][q].length; r++) {
+					System.out.print(array4[p][q][r] + " ");
+				}
+				// 改行
+				System.out.println();
+			}
+		}
 
 	}
 
