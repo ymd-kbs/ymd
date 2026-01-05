9d0680c4ef9bf6c7d563649abb1bdc5fa389bb63
ymd-kbs <kabashiraezpz@gmail.com>
2026-01-05 17:38:40 +0900

    課題①基本概念

---
+++ HelloWorld_YamadaFumiya/src/curriculum/e/Employee.java
@@ -0,0 +1,11 @@
+package curriculum.e;
+
+public class Employee {
+	// nameフィールド
+	public String name;
+
+	// work() メソッド
+	public void work() {
+		System.out.println(this.name + "は働いています。");
+	}
+}
---
+++ HelloWorld_YamadaFumiya/src/curriculum/e/main.java
@@ -0,0 +1,16 @@
+package curriculum.e;
+
+public class main {
+	public static void main(String[] args) {
+		/*
+		 * [課題①] 基本概念
+		 */
+		// Employee クラスのインスタンスを生成する
+		Employee emp = new Employee();
+		// name に山田太郎を設定する
+		emp.name = "山田太郎";
+		// work() を呼び出して出力する
+		emp.work();
+		
+	}
+}
