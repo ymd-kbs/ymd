8b98c49790e1d5d750308f0b54fc108f77a344ae
ymd-kbs <kabashiraezpz@gmail.com>
2026-01-15 02:56:04 +0900

    課題③継承

---
+++ HelloWorld_YamadaFumiya/src/curriculum/g/Employee.java
@@ -0,0 +1,29 @@
+package curriculum.g;
+
+public class Employee {
+
+	// プライベートフィールドの定義
+	private String employeeId;
+	private String name;
+
+	// employeeId の Setter
+	public void setEmployeeId(String id) {
+		this.employeeId = id;
+	}
+
+	// employeeId の Getter
+	public String getEmployeeId() {
+		return this.employeeId;
+	}
+
+	// name の Setter
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	// name の Getter
+	public String getName() {
+		return this.name;
+	}
+
+}
---
+++ HelloWorld_YamadaFumiya/src/curriculum/g/Main.java
@@ -0,0 +1,19 @@
+package curriculum.g;
+
+public class Main {
+
+	public static void main(String[] args) {
+		/*
+		 * [課題③] カプセル化
+		 */
+		// Employee オブジェクトを生成
+		Employee emp = new Employee();
+		// set メソッドを使って値を設定
+		emp.setEmployeeId("E002");
+		emp.setName("田中花子");
+		// get メソッドを使って値を取得し、出力
+		System.out.println("社員ID: " + emp.getEmployeeId() + ", 名前: " + emp.getName());
+
+	}
+
+}
