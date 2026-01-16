d4b13270cd6b892ee0a5fa416d064c0027e5f9e7
ymd-kbs <kabashiraezpz@gmail.com>
2026-01-16 22:08:47 +0900

    課題Q2　バトルプログラム修正後

--- HelloWorld_YamadaFumiya/src/curriculum_D/Player.java
+++ HelloWorld_YamadaFumiya/src/curriculum_D/Player.java
@@ -1,11 +1,12 @@
 package curriculum_D;
 
 class Player extends Character {
+	private static final java.util.Random RANDOM = new java.util.Random();
     public Player(String name) {
         // ステータスをランダム設定（HP: 30-80, AT: 5-25, SP: 1-10）
         super(name, 
-            new java.util.Random().nextInt(51) + 30, 
-            new java.util.Random().nextInt(21) + 5, 
-            new java.util.Random().nextInt(10) + 1);
+            RANDOM.nextInt(51) + 30, 
+            RANDOM.nextInt(21) + 5, 
+            RANDOM.nextInt(10) + 1);
     }
 }
