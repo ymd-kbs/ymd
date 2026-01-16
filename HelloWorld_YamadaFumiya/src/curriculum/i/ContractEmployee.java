910aaa4dae485b48db1e652087bd2e9b494f0aef
ymd-kbs <kabashiraezpz@gmail.com>
2026-01-16 18:14:14 +0900

    課題⑤ポリフォーリズム　修正後

--- HelloWorld_YamadaFumiya/src/curriculum/i/ContractEmployee.java
+++ HelloWorld_YamadaFumiya/src/curriculum/i/ContractEmployee.java
@@ -1,6 +1,9 @@
 package curriculum.i;
 
 public class ContractEmployee extends Employee {
+	// 給与の定数
+	private static final int HOURLY_RATE = 1000;
+
 	// コンストラクタ
 	public ContractEmployee(String id, String name) {
 		super(id, name);
@@ -9,8 +12,9 @@ public class ContractEmployee extends Employee {
 	// 契約社員用の計算
 	@Override
 	public int calculateDailyWage(int hoursWorked) {
-		int hourlyRate = 1000;
+		// 0制限
+		int hours = Math.max(0, hoursWorked);
 		// 時給計算
-		return hoursWorked * hourlyRate;
+		return hours * HOURLY_RATE;
 	}
 }
--- HelloWorld_YamadaFumiya/src/curriculum/i/FullTimeEmployee.java
+++ HelloWorld_YamadaFumiya/src/curriculum/i/FullTimeEmployee.java
@@ -1,19 +1,23 @@
 package curriculum.i;
 
 public class FullTimeEmployee extends Employee {
+	// 給与の定数
+	private static final int HOURLY_RATE = 1250;      // 時給
+    private static final double OVERTIME_RATIO = 1.25; // 残業倍率
+    private static final int REGULAR_HOURS_LIMIT = 8; // 定時時間
 	// コンストラクタ
 	public FullTimeEmployee(String id, String name) {
 		super(id, name);
 	}
-
 	// 正社員用の計算
 	@Override
 	public int calculateDailyWage(int hoursWorked) {
-		int hourlyRate = 1250;
-		int overtime = Math.max(0, hoursWorked - 8);
-		int regularHours = hoursWorked - overtime;
+		// 0制限
+		int hours = Math.max(0, hoursWorked);
+		int overtime = Math.max(0, hours - REGULAR_HOURS_LIMIT);
+		int regularHours = hours - overtime;
 		// 基本給 + (残業 * 1.25倍)
-		return (regularHours * hourlyRate) + (int) (overtime * hourlyRate * 1.25);
+		return (regularHours * HOURLY_RATE) + (int) (overtime * HOURLY_RATE * 1.25);
 	}
 
 }
